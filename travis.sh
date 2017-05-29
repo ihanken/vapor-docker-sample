4f7b820c1ac4ff51caf17d05a4ae09657ddf233a
ihanken <ihanken@bellsouth.net>
2017-05-29 12:16:11 -0500

    Changed to sh command.

--- travis.sh
+++ travis.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
     # This runs when a pull request is submitted.
-    /bin/bash ./run_tests.sh
+    sh ./run_tests.sh
 fi
 
 if [[ "$TRAVIS_BRANCH" == "develop" ]]; then
     # This runs on a merge into develop.
-    /bin/bash ./upload_development_container.sh
+    sh ./upload_development_container.sh
 fi
 
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
