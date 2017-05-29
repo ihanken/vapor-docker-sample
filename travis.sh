6d8867f6cdc68cdcaf451416133fe8883461aa41
ihanken <ihanken@bellsouth.net>
2017-05-29 12:09:26 -0500

    Trying a different way to execute a script.

--- travis.sh
+++ travis.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
     # This runs when a pull request is submitted.
-    ./run_tests.sh
+    /bin/bash ./run_tests.sh
 fi
 
 if [[ "$TRAVIS_BRANCH" == "develop" ]]; then
     # This runs on a merge into develop.
-    ./upload_development_container.sh
+    /bin/bash ./upload_development_container.sh
 fi
 
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
